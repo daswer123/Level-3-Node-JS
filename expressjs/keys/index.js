e7207d2db2eb62a773e7636266f55245720eafcd
daswer123 <daswerq123@gmail.com>
2020-09-26 11:58:46 +0300

    what is goin on

--- expressjs/keys/index.js
+++ expressjs/keys/index.js
@@ -1,5 +1,5 @@
 const keys ={
-    EMAIL_API_KEY : "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",
+    EMAIL_API_KEY : "SG.",
     MONGO_DB_URL  : "mongodb+srv://daswer_server:1XLpnSuGk5J8s6pE@cluster0.4jm4e.mongodb.net/shop-tutor?retryWrites=true&w=majority",
     SECRET_KEY    : "My secret code",
     EMAIL_ROOT_NAME : "mailservice@boldyrevdev.xyz",
