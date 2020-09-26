d87517938cc5332cb42df22910832909abbdff1f
daswer123 <daswerq123@gmail.com>
2020-09-26 12:23:47 +0300

    a little security

--- expressjs/keys/index.js
+++ expressjs/keys/index.js
@@ -2,7 +2,7 @@ const api = require("./email_api.js");
 
 const keys ={
     EMAIL_API_KEY : api.EMAIL_API_KEY,
-    MONGO_DB_URL  : "mongodb+srv://daswer_server:1XLpnSuGk5J8s6pE@cluster0.4jm4e.mongodb.net/shop-tutor?retryWrites=true&w=majority",
+    MONGO_DB_URL  : api.MONGO_DB_URL,
     SECRET_KEY    : "My secret code",
     EMAIL_ROOT_NAME : "mailservice@boldyrevdev.xyz",
     MAIN_URL : "http://localhost:3000/"
