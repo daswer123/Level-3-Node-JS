8caae430f6fced37c3b8b92aae222fab1c83cda4
daswer123 <daswerq123@gmail.com>
2020-09-28 21:28:19 +0300

    Start making Rest Api sevice

---
+++ rest-api/routes/todo.js
@@ -0,0 +1,20 @@
+const Router = require("express");
+const router = Router();
+
+router.get("/", (req,res) => {
+
+})
+
+router.post("/", (req,res) => {
+    
+})
+
+router.put("/", (req,res) => {
+    
+})
+
+router.delete("/", (req,res) => {
+    
+})
+
+module.exports = router
